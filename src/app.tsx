a37a1e4a921d39f8d93fc047c73eedda5950c318
Victor Martins <vitor_jhoul@hotmail.com>
2024-02-06 20:56:29 -0300

    configurando modal

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,11 @@ import { FormSearchNotes } from "./components/form-search-notes";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+const note = {
+  date: new Date(),
+  content: 'Hello World'
+}
+
 export function App() {
 
   return (
@@ -14,9 +19,11 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }} />
+
       </div>
 
     </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,39 @@
-export function NoteCard() {
+import * as Dialog from '@radix-ui/react-dialog'
+
+interface NoteCArdProps {
+  note: {
+    date: Date,
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCArdProps) {
   return (
-    <button className="rounded-md text-left outline-none bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300 ">
-        Há 2 dias
-      </span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias saepe sapiente numquam ratione dolore blanditiis quod accusantium, iste quasi cumque ullam, inventore ad! Rerum laborum modi soluta quos hic?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias saepe sapiente numquam ratione dolore blanditiis quod accusantium, iste quasi cumque ullam, inventore ad! Rerum laborum modi soluta quos hic?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestias saepe sapiente numquam ratione dolore blanditiis quod accusantium, iste quasi cumque ullam, inventore ad! Rerum laborum modi soluta quos hic?
-      </p>
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/50 to-black/0 pointer-events-none" />
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col outline-none bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300 ">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/50 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none '>
+          <div className="flex flex-1 flex-col gap-3 p-5 ">
+          <span className="text-sm font-medium text-slate-300 ">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
 
