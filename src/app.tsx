386f9906e1aa3b175d03562292b79acae371c299
Victor Martins <vitor_jhoul@hotmail.com>
2024-02-08 13:18:31 -0300

    feat: finalização de estilos

--- src/app.tsx
+++ src/app.tsx
@@ -50,13 +50,13 @@ export function App() {
   const filteredNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Logo nlw" />
       <FormSearchNotes handleSearch={handleSearch} />
 
       <div className="h-px bg-slate-700" ></div>
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {filteredNotes.map((note) => (
